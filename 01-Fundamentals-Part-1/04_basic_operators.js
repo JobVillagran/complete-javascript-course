b3fc3546d40178038ac5e2506bb8b8058a811767
JobVillagran <jobvillagran@gmail.com>
2024-01-22 09:18:55 -0600

    mod[basic_operators]

---
+++ 01-Fundamentals-Part-1/04_basic_operators.js
@@ -0,0 +1,71 @@
+/* eslint-disable max-len */
+// Math Operators
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+
+let x = 10 + 5;
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--; // x = x - 1
+console.log(x);
+
+// Comparison operators
+
+// We used them to produce a boolean value
+
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+// eslint-disable-next-line no-unused-vars
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+
+// Code Challenge
+
+/*
+CHALLENGE #1
+// eslint-disable-next-line max-len, max-len
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
+
+Your task is to write some code to help them:
+
+Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
+
+Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
+
+Log the value of BMIMark and BMIJohn to the console.
+
+BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
+
+TEST DATA: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.
+
+*/
+
+/* Write your code below. Good luck! 🙂 */
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+const markHigherBMI = BMIMark >= BMIJohn;
+console.log(markHigherBMI);
