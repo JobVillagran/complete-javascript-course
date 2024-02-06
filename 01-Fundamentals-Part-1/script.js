c54f3b9d7aacddcd65cc3125b2c78e0f42186393
JobVillagran <jobvillagran@gmail.com>
2024-02-05 21:48:29 -0600

    feat[ternary_operator]

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -6,3 +6,8 @@ console.log(40 + 8 + 23 - 10);
 
 */
 
+const age = 19;
+age >= 18 ? console.log('I like to drink beer') : console.log('I like to drink water');
+
+const drink = age >= 18 ? 'Beer' : 'Water';
+console.log(drink);
