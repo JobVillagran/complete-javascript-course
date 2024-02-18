34596703e7a46562adc4d4e31b270115731a9957
JobVillagran <jobvillagran@gmail.com>
2024-02-18 06:36:32 -0600

    feat[strict_mode]

---
+++ 02-Fundamentals-Part-2/01 _strict_mode.js
@@ -0,0 +1,9 @@
+'use strict';
+// Can be helpful for debugging on the console
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,7 @@
+/* eslint-disable max-len */
+/* const js = 'boring';
+if (js === 'amazing') alert('JavaScriot is FUN!');
+
+console.log(40 + 8 + 23 - 10);
+
+*/
