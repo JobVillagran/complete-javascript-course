9a2f0a9901bbda956287b256f0da0bea1579807e
JobVillagran <jobvillagran@gmail.com>
2024-02-27 05:55:18 -0600

    feat[functions_calling_other_functions]

---
+++ 02-Fundamentals-Part-2/05_functions_calling_other_functions.js
@@ -0,0 +1,15 @@
+/* eslint-disable max-len */
+/* eslint-disable require-jsdoc */
+
+function cutFruitPieces(fruit) {
+  return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+  return juice;
+}
+
+console.log(fruitProcessor(2, 3));
